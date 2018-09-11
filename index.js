d50c227ab34ad404357bf605d4861e05a892b485
YanC Lin <yonalin1@gmail.com>
2018-09-11 21:05:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function init() {
   });
   Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML);
   Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML);
-  
+
   let formTemplate = document.getElementById("recipe-form-template").innerHTML;
   let template = Handlebars.compile(formTemplate);
   document.getElementsByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'});
