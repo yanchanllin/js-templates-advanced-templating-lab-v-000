aae977ac7b2a7f0f0d8e736ab674cc46c1f20cd5
YanC Lin <yonalin1@gmail.com>
2018-09-11 20:03:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function init() {
   //put any page initialization/handlebars initialization here
-}
+handlebars.registerHelper('displayIngredient', function(ingredient){
+  return new Handlebars.SafeString('<li name="ingredientList">' + ingredient + '</li>');
+});
+
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
